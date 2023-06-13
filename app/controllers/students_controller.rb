0adf1670bd9f14819b64a08c0e310b9b626edf22
Megan Dacus <meganjustice0609@gmail.com>
2023-06-13 17:26:28 +1000

    Completed bonus

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,12 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    students =  if params[:name]
+                  name = params[:name]
+                  Student.where("first_name LIKE ? OR last_name LIKE ?", name, name)
+                else
+                  Student.all
+                end
     render json: students
   end
 
